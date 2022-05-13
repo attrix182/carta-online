50cc43479c9061b1f2e403bf4310b1cb6b195ece
Jacqueline Maquez <jmarquez@valkiria>
2022-05-13 14:04:46 -0300

    agregue funcion para agregar bebidas

--- src/app/pages/carta/carta.component.ts
+++ src/app/pages/carta/carta.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-carta',
   templateUrl: './carta.component.html',
-  styleUrls: ['./carta.component.css']
+  styleUrls: ['./carta.component.css'],
 })
 export class CartaComponent implements OnInit {
-
-  constructor() { }
+  constructor(private storage: StorageService) {}
 
   ngOnInit(): void {
+    this.getMenuItems();
   }
 
+  getMenuItems(): any {
+    this.storage.GetAll('items').subscribe((items) => {
+      console.log(items);
+    });
+  }
 }
